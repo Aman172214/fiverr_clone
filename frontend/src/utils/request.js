92186e480e05a7ed43a4a6565a70d72985dde90a
Aman <Sharmaamqn@gmail.com>
2024-01-15 16:32:58 +0530

    redeploy

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "https://fiverr-clone-api.vercel.app",
+  baseURL: "https://fiverr-clone-api-eotf.onrender.com",
   withCredentials: true,
 });
 
