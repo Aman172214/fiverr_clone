478c939c35883a351e1b8777dfc2265007aadd13
Aman <Sharmaamqn@gmail.com>
2024-01-15 18:13:03 +0530

    fix cookie error

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -36,7 +36,10 @@ export const login = async (req, res, next) => {
     );
 
     const { password, ...info } = user._doc;
-    res.cookie("accessToken", token).status(200).send(info);
+    res
+      .cookie("accessToken", token, { sameSite: "none", secure: true })
+      .status(200)
+      .send(info);
   } catch (err) {
     next(err);
   }
